dc18d2ee8e609e2e03e2c52d3826ab5e94ddacbe
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-07 12:50:07 +0300

    added admin profile view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
@@ -19,6 +20,12 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
-    } //end method destroy
+    } //end destroy method
 
+    public function profile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }//end  profile method
 }
