c699da9708c762a54c1d46e5b2baa3dc41d33b05
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-11 05:41:18 +0300

    added category blog details view page to front end

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -143,4 +143,13 @@ class BlogController extends Controller
         $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
         return view('frontend.blog_details', compact('blogs', 'allblogs', 'categories'));
     } // End Method
+
+    public function CategoryBlog($id)
+    {
+        $blogpost = Blog::where('blog_category_id', $id)->orderBy('id', 'DESC')->get();
+        $allblogs = Blog::latest()->limit(5)->get();
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        $categoryname = BlogCategory::findOrFail($id);
+        return view('frontend.cat_blog_details', compact('blogpost', 'allblogs', 'categories', 'categoryname'));
+    } // End Method
 }
