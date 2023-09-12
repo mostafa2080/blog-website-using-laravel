839f3e43354bf3c7c4c2feaf47585b70677988f8
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-12 17:09:00 +0300

    applied form validation to blog category edit

--- resources/views/admin/blog_category/blog_category_edit.blade.php
+++ resources/views/admin/blog_category/blog_category_edit.blade.php
@@ -1,50 +1,84 @@
 @extends('admin.admin_master')
 @section('admin')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<div class="page-content">
-<div class="container-fluid">
-
-<div class="row">
-<div class="col-12">
-    <div class="card">
-        <div class="card-body">
-
-            <h4 class="card-title">Edit Blog Category Page </h4> <br><br>
-
-            <form method="post" action="{{ route('update.blog.category',$blogcategory->id) }}" >
-                @csrf
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 
 
+    <div class="page-content">
+        <div class="container-fluid">
 
-            <div class="row mb-3">
-                <label for="example-text-input" class="col-sm-2 col-form-label">Blog Category Name</label>
-                <div class="col-sm-10">
-                    <input name="blog_category" class="form-control" type="text" value="{{ $blogcategory->blog_category }}" id="example-text-input">
-                    @error('blog_category')
-                    <span class="text-danger"> {{ $message }} </span>
-                    @enderror
-                </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <h4 class="card-title">Edit Blog Category Page </h4> <br><br>
+
+                            <form id="editForm" method="post"
+                                action="{{ route('update.blog.category', $blogcategory->id) }}">
+                                @csrf
+
+
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Blog Category
+                                        Name</label>
+                                    <div class="col-sm-10">
+                                        <input name="blog_category" placeholder="" class="form-control" type="text"
+                                            value="{{ $blogcategory->blog_category }}" id="example-text-input">
+                                    </div>
+                                </div>
+                                <!-- end row -->
+
+
+                                <input type="submit" class="btn btn-info waves-effect waves-light"
+                                    value="Update Blog Category">
+                            </form>
+
+
+
+                        </div>
+                    </div>
+                </div> <!-- end col -->
             </div>
-            <!-- end row -->
-
-
-<input type="submit" class="btn btn-info waves-effect waves-light" value="Update Blog Category">
-            </form>
 
 
 
         </div>
     </div>
-</div> <!-- end col -->
-</div>
 
 
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#editForm').validate({
+                rules: {
+                    blog_category: {
+                        required: true,
+                        minlength: 3,
+                    },
 
-</div>
-</div>
 
+                },
+                messages: {
+                    blog_category: {
+                        required: 'Blog Category Name Can not be empty',
+                    },
 
-
-
-@endsection 
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    let n = element.attr("name");
+                    if (n == "blog_category")
+                        element.attr("placeholder", "Blog Category can not be empty");
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                },
+            });
+        });
+    </script>
+@endsection
